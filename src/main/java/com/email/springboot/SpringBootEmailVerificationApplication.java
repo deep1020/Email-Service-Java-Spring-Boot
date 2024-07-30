293d22a99a3504d59927e0e59f469e5ce4859cc4
Deep Patel <drp037@gmail.com>
2024-07-30 12:50:57 -0700

    Commit

---
+++ src/main/java/com/email/springboot/SpringBootEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.email.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email/springboot/SpringBootEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmailVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
